c05ff718f7d4c9a14cb760fbb375a017970067a6
bulelaniskhikhi <bulelaniskhikhi@gmail.com>
2022-08-26 09:14:10 +0200

    second try

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('./config/dbconn');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 
