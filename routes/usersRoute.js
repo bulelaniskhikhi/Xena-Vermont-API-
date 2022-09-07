3de903e93879b178fb117817fea178cdd013d45d
bulelaniskhikhi <bulelaniskhikhi@gmail.com>
2022-09-07 16:05:03 +0200

    update

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const con = require('../config/dbconn');
-const { param } = require('express/lib/request');
+const {
+    param
+} = require('express/lib/request');
 const router = express.Router();
-const bcrypt = require ('bcrypt');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { json } = require('express/lib/response');
+const {
+    json
+} = require('express/lib/response');
 
 router.get('/', (req, res) => {
-    try{
-      const strQry = `SELECT * FROM users`;
+    try {
+        const strQry = `SELECT * FROM users`;
 
-      con.query(strQry, (err, results) => {
-          if (err) throw err;
+        con.query(strQry, (err, results) => {
+            if (err) throw err;
 
-          res.json({
-              results: results,
-              msg: "All Users Shown",
-          });
-      });
+            res.json({
+                results: results,
+                msg: "All Users Shown",
+            });
+        });
     } catch (error) {
         res.status(400).json({
             error
@@ -27,7 +31,7 @@ router.get('/', (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-    try{
+    try {
         const strQry = `SELECT * FROM users WHERE id = ${req.params.id}`;
 
         con.query(strQry, (err, results) => {
@@ -45,43 +49,47 @@ router.get("/:id", (req, res) => {
     }
 });
 
-router.patch('/', bodyParser.json(), (req, res) => {
-    try{
-      const strQry = `SELECT * FROM users where userEmail = ?`;
+router.patch('/', bodyParser.json(), async (req, res) => {
+    try {
+        const strQry = `SELECT * FROM users where userEmail = ?`;
 
-      con.query(strQry, req.body.userEmail,  (err, results) => {
-          if (err) throw err;
-        if (results.length>0) {
-            bcrypt.compare(req.body.userPassword, results[0].userPassword, (err, check) => {
-                if (check) {
+        con.query(strQry, req.body.userEmail, async (err, results) => {
+            if (err) throw err;
+            if (results.length > 0) {
+
+                let check =  await bcrypt.compare(req.body.userPassword, results[0].userPassword)
+                // res.send(check)
+                if (check === true) {
+
                     const payload = {
                         userName: results[0].userName,
                         userEmail: results[0].userName,
                         userPassword: results[0].userPassword,
                         userRole: results[0].userRole,
                     }
-                    jwt.sign(results[0], process.env.jwtSecret, (err, wd) =>{
-                       res.json({
-                           token: wd
-                           
-                       })
+                    jwt.sign(payload, process.env.jwtSecret, (err, wd) => {
+                        res.json({
+                            payload,
+                            token: wd
+
+                        })
                     })
-                } else{
+                } else {
                     res.json({
                         msg: "password is wrong"
                     })
                 }
-            })
-        }else {
-            res.json({
-              results: results,
-              msg: "All Users Shown",
-          });
-        }
+
+            } else {
+                res.json({
+                    results: results,
+                    msg: "All Users Shown",
+                });
+            }
 
 
-          
-      });
+
+        });
     } catch (error) {
         res.status(400).json({
             error
@@ -90,30 +98,32 @@ router.patch('/', bodyParser.json(), (req, res) => {
 });
 
 router.post("/", bodyParser.json(), async (req, res) => {
-    try{
+    try {
         const strQry = `SELECT * FROM users WHERE userEmail = ?`
 
         con.query(strQry, req.body.userEmail, async (err, results) => {
-            if(err) throw err;
-            if (results.length>0 ) {
+            if (err) throw err;
+            if (results.length > 0) {
                 res.json({
                     msg: "email already exists"
                 })
             } else {
-                req.body.userPassword= await bcrypt.hash(req.body.userPassword, 10)
+                req.body.userPassword = await bcrypt.hash(req.body.userPassword, 10)
                 const insertstrQry = `INSERT INTO users (userName, userPassword, userEmail, userRole) VALUES (?,?,?,?)`
 
                 con.query(insertstrQry, [req.body.userName, req.body.userPassword, req.body.userEmail, req.body.userRole], (err, insertResults) => {
-                    if(err) throw err;
-            
-                res.json({
+                    if (err) throw err;
+
+                    res.json({
                         msg: "register successful "
-                    }) 
-                } )
+                    })
+                })
             }
         })
-    }catch(e) {
-        res.status(400).json({err: e.message})
+    } catch (e) {
+        res.status(400).json({
+            err: e.message
+        })
     }
 })
 
