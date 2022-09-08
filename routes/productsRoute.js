24699bc1f17989b4db92ad49d05727e3f3b33f52
bulelaniskhikhi <bulelaniskhikhi@gmail.com>
2022-09-08 11:31:23 +0200

    update

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const con = require('../config/dbconn');
-const { param } = require('express/lib/request');
+const {
+    param
+} = require('express/lib/request');
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    try{
-      const strQry = `SELECT * FROM products`;
+    try {
+        const strQry = `SELECT * FROM products`;
 
-      con.query(strQry, (err, results) => {
-          if (err) throw err;
+        con.query(strQry, (err, results) => {
+            if (err) throw err;
 
-          res.json({
-              results: results,
-              msg: "All Products Shown",
-          });
-      });
+            res.json({
+                results: results,
+                msg: "All Products Shown",
+            });
+        });
     } catch (error) {
         res.status(400).json({
             error
@@ -24,7 +26,7 @@ router.get('/', (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-    try{
+    try {
         const strQry = `SELECT * FROM products WHERE id = ${req.params.id}`;
 
         con.query(strQry, (err, results) => {
@@ -43,7 +45,7 @@ router.get("/:id", (req, res) => {
 });
 
 router.get("/rating/:esrbRating", (req, res) => {
-    try{
+    try {
         const strQry = `SELECT * FROM products WHERE esrbRating = ?`;
 
         con.query(strQry, req.params.esrbRating, (err, results) => {
@@ -62,7 +64,7 @@ router.get("/rating/:esrbRating", (req, res) => {
 });
 
 router.get("/genre/:genre", (req, res) => {
-    try{
+    try {
         const strQry = `SELECT * FROM products WHERE genre = ?`;
 
         con.query(strQry, req.params.genre, (err, results) => {
@@ -79,5 +81,129 @@ router.get("/genre/:genre", (req, res) => {
         })
     }
 });
+
+router.put("/:id", bodyParser.json(), (req, res) => {
+    try {
+        const {
+            id,
+            title,
+            romajiTitle,
+            kanjiTitle,
+            frontCoverImg,
+            backCoverImg,
+            frontDiscCoverImg,
+            backDiscCoverimg,
+            realisedDate,
+            serialCode,
+            studioDev,
+            studioPub,
+            priceRands,
+            priceYens,
+            gameDesc,
+            esrbRating,
+            esrbRatingImg,
+            genre,
+            features
+        } = req.body
+        const str = `UPDATE products SET ? WHERE id = ${req.params.id}`
+
+        const product = {
+            // bd.
+            id,
+            title,
+            romajiTitle,
+            kanjiTitle,
+            frontCoverImg,
+            backCoverImg,
+            frontDiscCoverImg,
+            backDiscCoverimg,
+            realisedDate,
+            serialCode,
+            studioDev,
+            studioPub,
+            priceRands,
+            priceYens,
+            gameDesc,
+            esrbRating,
+            esrbRatingImg,
+            genre,
+            features
+        }
+
+        con.query(str, product, (err, results) => {
+            if (err) throw err;
+
+            res.json({
+                results,
+                msg: "updated product"
+            })
+        })
+    } catch (error) {
+        throw error
+    }
+});
+
+router.delete("/:id", (req, res) => {
+    try {
+        const str = `DELETE FROM products WHERE id = ${req.params.id}`;
+
+        con.query(str, (err, results) => {
+            if (err) throw err;
+
+            res.json({
+                results,
+                msg : "item deleted"
+            })
+        })
+    } catch (error) {
+        throw error
+    }
+});
+
+// add product
+router.post("/", bodyParser.json(), (req, res) => {
+    try {
+      const products = req.body;
+      const strQry = `INSERT INTO products (title, romajiTitle, kanjiTitle, frontCoverImg, backCoverImg, frontDiscCoverImg, backDiscCoverimg, realisedDate, serialCode, studioDev, studioPub, priceRands, priceYens, gameDesc, esrbRating, esrbRatingImg, genre, features) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+  
+      con.query(
+        strQry,
+        [
+          products.title,
+          products.romajiTitle,
+          products.kanjiTitle,
+          products.frontCoverImg,
+          products.backCoverImg,
+          
+          products.frontDiscCoverImg,
+          products.backDiscCoverimg,
+          products.realisedDate,
+          products.serialCode,
+          products.studioDev,
+          products.studioPub,
+
+          products.priceRands,
+          products.priceYens,
+          products.gameDesc,
+          products.esrbRating,
+          products.esrbRatingImg,
+
+          products.genre,
+          products.features,
+          
+        ],
+        (err, results) => {
+          if (err) throw err;
+          res.status(200).json({
+            msg: "Product Added",
+          });
+        }
+      );
+    } catch (error) {
+      res.status(200).json({
+        error,
+      });
+    }
+  });
 
 module.exports = router
